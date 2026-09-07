6a3c569b52e1802e1d99d79a89913ac22556e416
Bakhtiar76 <badarosabbar76@gmail.com>
2026-09-07 20:09:58 +0500

    chore: prune main to production-ready theme code only

--- local/wp.ps1
+++
@@ -1,8 +0,0 @@
-# Wrapper for running WP-CLI against the local Docker WordPress site.
-# Usage: .\local\wp.ps1 plugin list
-#        .\local\wp.ps1 theme activate easy-quran-classes-child
-param(
-    [Parameter(ValueFromRemainingArguments = $true)]
-    [string[]]$WpArgs
-)
-docker compose -f "$PSScriptRoot\docker-compose.yml" run --rm wpcli @WpArgs
